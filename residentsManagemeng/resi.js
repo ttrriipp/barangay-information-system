2a0c446c63d7284452401a7b6d697683b234626c
ela874 <mariasampang474@gmail.com>
2025-05-09 08:09:44 +0800

    resident managemaent html,css and javascript
    pacheck nalang po

---
+++ residentsManagemeng/resi.js
@@ -0,0 +1,9 @@
+function addResident() {
+    alert("Add New Resident button clicked!");
+}
+function editResident() {
+    alert("Edit Resident button clicked!"); 
+}
+function deleteResident() {
+    alert("Delete Resident button clicked!");
+}
