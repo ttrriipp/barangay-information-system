6fd43ba25d6fa4d8bcd833b2dddb28718bffb1e5
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-05-10 16:12:00 +0800

    wew

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// Entry point - redirect to login page
+header("Location: pages/login.php");
+exit();
--- pages/register.php
+++ pages/register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Account</title>
-    <link rel="stylesheet" href="createacc.css">
+    <link rel="stylesheet" href="../assets/css/register.css">
 </head>
 <body>
     <div class="container">
