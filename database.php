17a8a91b0d52d30de9922f367c539d9739b8dcbc
freezy29 <189191275+freezy29@users.noreply.github.com>
2025-05-11 14:20:41 +0800

    "added database connection and controller"

---
+++ database.php
@@ -0,0 +1,20 @@
+<?php
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "barangay_infomanage";
+
+    $conn = "";
+
+    try{
+    $conn = mysqli_connect($db_server,
+                           $db_user,
+                           $db_pass,
+                           $db_name);
+    }
+   catch(mysqli_sql_exception){
+    echo "Could not Connect to the database";
+   }
+
+
+?>
