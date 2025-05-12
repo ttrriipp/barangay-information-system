bccef3b2c908b6ad8a2e078947004849c6b2ec0a
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-05-12 19:40:42 +0800

    contrullerr

--- controllers/register.php
+++ controllers/register.php
@@ -2,6 +2,8 @@
     include("../database.php");
 ?>
 
+<?php require("../pages/register.php") ?>
+
 <?php
     if( $_SERVER["REQUEST_METHOD"] == "POST")   {
         $user = filter_input(INPUT_POST, "user", FILTER_SANITIZE_SPECIAL_CHARS);
