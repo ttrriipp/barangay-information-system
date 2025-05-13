4e83c18a8f3bc35425a2e168dac606cbd9e438b8
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-05-13 17:50:48 +0800

    rawr

--- pages/register.php
+++ pages/register.php
@@ -9,19 +9,17 @@
                 <label for="username">Username:</label>
                 <input type="text" id="username" placeholder="Username" name="user" required>
                 
-                <label for="password">Create Password:</label>
+                <label for="password">Password:</label>
                 <input type="password" id="password" placeholder="Create Password" name="pass" required>
-                
-                <div class="show-password-container">
-                    <input type="checkbox" id="showPassword">
-                    <label for="showPassword">Show password</label>
-                </div>
                 
                 <label for="retypePassword">Re-type Password:</label>
                 <input type="password" id="retypePassword" placeholder="Re-type Password" required>
                 
-                <button type="submit" id="submit_button" name="submit" >Create Account</button>
+                <button type="submit" id="submit_button" name="submit">Create Account</button>
             </form>
+            <div class="back-link">
+                <a href="login.php" style="color: #0066ff; text-decoration: none; font-size: 1em; display: inline-block; margin-top: 15px;">Back to Login</a>
+            </div>
         </div>
         <div class="logo-container">
             <h2>Cupang West Information Management System</h2>
